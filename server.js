35384bb96c9bdf38dd291c22079823fc0cee5b92
Tulebaeva Marzhan <241398@astanait.edu.kz>
2025-12-14 18:03:59 +0500

    add creativity page

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const app = express();
+
 app.use(express.static('public'));
 app.use('/images', express.static('images'));
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
-app.get('/about', (req, res) => {
-  res.sendFile(__dirname + '/views/about.html');
+
+app.get('/creativity', (req, res) => {
+  res.sendFile(__dirname + '/views/creativity.html');
 });
 
-app.listen(3000, () => console.log('Server running on http://localhost:3000'));
+app.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
