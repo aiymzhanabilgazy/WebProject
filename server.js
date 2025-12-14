b3511504fd41bc1bdc6372f155383f0d19319f94
Tulebaeva Marzhan <241398@astanait.edu.kz>
2025-12-14 19:26:02 +0500

    add locations and update banner

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const app = express();
-
 app.use(express.static('public'));
 app.use('/images', express.static('images'));
-
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
-
+app.get('/about', (req, res) => {
+  res.sendFile(__dirname + '/views/about.html');
+});
 app.get('/creativity', (req, res) => {
   res.sendFile(__dirname + '/views/creativity.html');
 });
-
-app.listen(3000, () => {
-  console.log('Server running on http://localhost:3000');
+app.get('/locations', (req, res) => {
+  res.sendFile(__dirname + '/views/locations.html');
 });
+
+app.listen(3000, () => console.log('Server running on http://localhost:3000'));
