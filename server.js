8d1ce57c1bbe8236650d2756bab3b2f8112bfb40
Tulebaeva Marzhan <241398@astanait.edu.kz>
2025-12-21 01:14:47 +0500

    Add contact form

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const app = express();
+
 app.use(express.static('public'));
 app.use('/images', express.static('images'));
+app.use(express.urlencoded({extended:true}))
+app.post('/submit',(req,res)=>{
+  const{name,email,message}=req.body;
+
+  res.send (`
+    <h2>Form submitted</h2>
+    <p>Name:${name}</p>
+    <p>Email:${email}</p>
+    <p>Message:${message}</p>
+    <a href="/contact.html">Back</a>
+    `);
+});
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
@@ -14,5 +27,9 @@ app.get('/creativity', (req, res) => {
 app.get('/locations', (req, res) => {
   res.sendFile(__dirname + '/views/locations.html');
 });
+app.get('/contact', (req, res) => {
+  res.sendFile(__dirname + '/views/contact.html');
+});
+
 
 app.listen(3000, () => console.log('Server running on http://localhost:3000'));
