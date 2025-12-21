6692de53c090cef13a483db5c881d1ce97a612e9
Tulebaeva Marzhan <241398@astanait.edu.kz>
2025-12-21 13:01:10 +0500

    update js

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
-const fs = require ('fs');
+const fs = require('fs');
 const app = express();
 
 app.use(express.static('public'));
 app.use('/images', express.static('images'));
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.post('/contact',(req,res)=>{
-  const{fname,lname,email,message}=req.body;
+app.post('/contact', (req, res) => {
+  const { fname, lname, email, message } = req.body;
 
   const formData = {
     firstName: fname,
@@ -16,43 +16,53 @@ app.post('/contact',(req,res)=>{
     message: message,
     date: new Date().toISOString()
   };
+
   fs.readFile('data.json', 'utf8', (err, data) => {
     let entries = [];
-    
+
     if (!err && data) {
       entries = JSON.parse(data);
     }
-    entries.push(newEntry);
-    
-    fs.writeFile('data.json', JSON.stringify(formData, null, 2),
-    (err) => {
-      if (err) {
-        res.send('<h2>Error saving data</h2>');
-      } else {
-        res.send(`
-          <h2>Form submitted successfully</h2>
-          <p>First Name: ${fname}</p>
-          <p>Last Name: ${lname}</p>
-          <p>Email: ${email}</p>
-          <p>Message: ${message}</p>
-          <a href="/contact">Back</a>`);
+
+    entries.push(formData);
+
+    fs.writeFile(
+      'data.json',
+      JSON.stringify(entries, null, 2),
+      (err) => {
+        if (err) {
+          res.send('<h2>Error saving data</h2>');
+        } else {
+          res.send(`
+            <h2>Form submitted successfully</h2>
+            <p>First Name: ${fname}</p>
+            <p>Last Name: ${lname}</p>
+            <p>Email: ${email}</p>
+            <p>Message: ${message}</p>
+            <a href="/contact">Back</a>
+          `);
         }
       }
-     );
-    });
+    );
+  });
+}); 
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
+
 app.get('/about', (req, res) => {
   res.sendFile(__dirname + '/views/about.html');
 });
+
 app.get('/creativity', (req, res) => {
   res.sendFile(__dirname + '/views/creativity.html');
 });
+
 app.get('/locations', (req, res) => {
   res.sendFile(__dirname + '/views/locations.html');
 });
+
 app.get('/contact', (req, res) => {
   res.sendFile(__dirname + '/views/contact.html');
 });
@@ -64,6 +74,6 @@ app.use((req, res) => {
   `);
 });
 
-
-
-app.listen(3000, () => console.log('Server running on http://localhost:3000'));
+app.listen(3000, () =>
+  console.log('Server running on http://localhost:3000')
+);
